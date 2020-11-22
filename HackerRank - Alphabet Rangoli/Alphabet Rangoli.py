91e88f0a3f4bc52b874ee55760cdd2f63b9009dd
Alisher Togayev <38793933+Alisherka7@users.noreply.github.com>
2020-11-22 01:26:43 -0800

    Update Alphabet Rangoli.py

--- HackerRank - Alphabet Rangoli/Alphabet Rangoli.py
+++ HackerRank - Alphabet Rangoli/Alphabet Rangoli.py
@@ -1,4 +1,5 @@
 import string
+
 alpha = string.ascii_lowercase
 
 def print_rangoli(size):
@@ -8,7 +9,6 @@ def print_rangoli(size):
         L.append((s[::-1]+s[1:]).center(4*size-3, "-"))
     print('\n'.join(L[:0:-1]+L))
 
-
-if __name__ == '__main__':
-    n = int(input())
-    print_rangoli(n)
+    
+n = int(input())
+print_rangoli(n)
