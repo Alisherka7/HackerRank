9949b1a3595ff922e8befdc180a12c568e423f09
Alisher Togayev <38793933+Alisherka7@users.noreply.github.com>
2020-11-22 01:56:17 -0800

    Update Designer Door Mat.py

--- HackerRank - Designer Door/Designer Door Mat.py
+++ HackerRank - Designer Door/Designer Door Mat.py
@@ -16,6 +16,7 @@ for i in range(a//2):
 
 print()
 
+# =======================================================================
 # n, m = map(int,input().split())
 # pattern = [('.|.'*(2*i + 1)).center(m, '-') for i in range(n//2)]
 # print('\n'.join(pattern + ['WELCOME'.center(m, '-')] + pattern[::-1]))
